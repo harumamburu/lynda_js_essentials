9df86764d97a6b31f51b6e9eb616eec88ccbf815
harumamburu <nikolay.kovalev92@gmail.com>
2018-01-09 19:33:55 +0300

    preserve commit for the 'Clock' project

---
+++ Smooth-Clock/script.js
@@ -0,0 +1,46 @@
+const HAND_HOUR = document.querySelector("#hour");
+const HAND_MINUTE = document.querySelector("#minute");
+const HAND_SECOND = document.querySelector("#second");
+
+function calculateSecondsDegrees(second) {
+  return second * 6; // 360 / 60 - 1 second takes 6 degrees step
+}
+function calculateMinutesDegrees(minute, second) {
+  // minute hand will be moving slowly towards the next minute
+  // as the second hand progresses
+  return minute * 6 + calculateSecondsDegrees(second) / 60;
+}
+function calculateHoursDegrees(hour, minute) {
+  // hour hand also will be adjusted smoothly
+  // with the progression of the minute hand
+  return hour * 30 + minute * 0.5; // 360 / 60 / 12 - the hour hand will be
+  // moved for 0.5 per minute
+}
+
+function getRotationStyle(degree) {
+  return "rotate(" + degree + "deg)";
+}
+function ammendRotation(rotation) {
+  var correction = 0;
+
+  return function(rotation) {
+    correction += rotation % 360 == 0 ? 360 : 0;
+    return rotation + correction;
+  }
+}
+
+var hrRotationCorrector = ammendRotation(0);
+var minRotationCorrector = ammendRotation(0);
+var secRotationCorrector = ammendRotation(0);
+function rotateClock() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  HAND_HOUR.style.transform = getRotationStyle(hrRotationCorrector(calculateHoursDegrees(hours, minutes)));
+  HAND_MINUTE.style.transform = getRotationStyle(minRotationCorrector(calculateMinutesDegrees(minutes, seconds)));
+  HAND_SECOND.style.transform = getRotationStyle(secRotationCorrector(calculateSecondsDegrees(seconds)));
+}
+
+var interval = setInterval(rotateClock, 1000);
